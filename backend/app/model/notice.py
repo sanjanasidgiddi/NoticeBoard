2949df0b958df2c09b905047987401e51de765c7
Sanjana Sidgiddi <sansid247700@gmail.com>
2026-08-29 20:07:49 -0400

    Add Notice Structure

---
+++ backend/app/model/notice.py
@@ -0,0 +1,16 @@
+from datetime import date as Date
+
+from pydantic import BaseModel, Field
+
+class NoticeCreate(BaseModel):
+    title: str = Field(min_length=1, max_length=100)
+    message: str = Field(min_length=1, max_length=1000)
+    author: str = Field(min_length=1, max_length=100)
+    date: Date
+
+
+class NoticeUpdate(BaseModel):
+    title: str | None = Field(default=None, min_length=1, max_length=100)
+    message: str | None = Field(default=None, min_length=1, max_length=1000)
+    author: str | None = Field(default=None, min_length=1, max_length=100)
+    date: Date | None = None
